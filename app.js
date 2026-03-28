0c518ee642c2f0b6cf9d4d409fd009ba79d84f35
Uxo Pousa <uxopousa@gmail.com>
2026-03-28 23:38:00 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.set('view engine','ejs');
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+    res.render('index');
+});
+
+app.listen(3000, () => {
+    console.log("Servidor iniciado en http://localhost:3000");
+});
