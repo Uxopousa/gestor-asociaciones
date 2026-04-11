589c9443ade5f6b9c8eb8673387f561468947386
Uxo Pousa <uxopousa@gmail.com>
2026-04-11 22:43:00 +0200

    prueba

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+//Archivos públicos
+app.use(express.static(path.join(__dirname, "public")));
+
 // Rutas
 const indexRouter = require("./routes/index");
+const authRouter = require("./routes/auth");
 
 app.use("/", indexRouter);
-
+app.use ("/login",authRouter);
 // Servidor
 app.listen(3000, () => {
     console.log("Servidor iniciado en http://localhost:3000");
